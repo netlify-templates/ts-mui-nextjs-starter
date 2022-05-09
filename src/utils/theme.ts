2a53fdde8505c74d3ebd894c5ac8ebefda12314a
tomasbankauskas <tbdizainas@gmail.com>
2022-05-09 11:15:27 +0300

    updated styles

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -9,8 +9,23 @@ let theme = createTheme({
     },
     secondary: {
       main: themeStyle.secondaryColor ?? '#F65458',
-    }
+    },
+    text: {
+      primary: themeStyle.mode === 'dark' ? '#fff' : '#02001d',
+      secondary: themeStyle.mode === 'dark' ? '#979797' : '#374151',
+    },
   },
+  typography: {
+    h1: {
+      fontWeight: 500,
+    },
+    h2: {
+      fontWeight: 500,
+    },
+    h3: {
+      fontWeight: 500,
+    }
+  }
 });
 
 theme = responsiveFontSizes(theme);
