ac11e8d31cd11aa2b8849513a11b389076a43459
tomasbankauskas <tbdizainas@gmail.com>
2022-06-28 14:23:35 +0300

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
         autoPrerender: false
     },
     webpack: (config) => {
-        config.watchOptions.ignored.push('**/content/**');
+        config.watchOptions.ignored.push('/content/');
         return config;
     }
 };
